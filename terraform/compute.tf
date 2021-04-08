b5f6e5b1900023f3d12b4d3d1b1c15b75df34d04
vilasvarghese <49194047+vilasvarghese@users.noreply.github.com>
2021-04-08 11:04:10 +0530

    Update compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "jenkins-instance" {
   ami             = "${data.aws_ami.amazon-linux-2.id}"
   instance_type   = "t2.medium"
   key_name        = "${var.keyname}"
+  private_key_path = "${var.keypath}"
   #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
