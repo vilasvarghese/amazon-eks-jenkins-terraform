43f6e682c92cd3ca035aba14c6219e7f40041adf
vilasvarghese <49194047+vilasvarghese@users.noreply.github.com>
2021-04-08 17:24:55 +0530

    Update install_jenkins.sh

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -32,3 +32,71 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
+
+echo "Install Kubernetes Master"
+#!/bin/bash
+
+# Update hosts file
+echo "[TASK 1] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+192.168.253.130 kmaster kmaster
+192.168.253.131 kworker1 kworker1
+192.168.253.132 kworker2 kworker2
+EOF
+
+# Disable SELinux
+echo "[TASK 4] Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+# Stop and disable firewalld
+echo "[TASK 5] Stop and Disable firewalld"
+systemctl disable firewalld >/dev/null 2>&1
+systemctl stop firewalld
+
+# Add sysctl settings
+echo "[TASK 6] Add sysctl settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+# Disable swap
+echo "[TASK 7] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Add yum repo file for Kubernetes
+echo "[TASK 8] Add yum repo file for kubernetes"
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Install Kubernetes
+echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
+yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+
+# Start and Enable kubelet service
+echo "[TASK 10] Enable and start kubelet service"
+systemctl enable kubelet >/dev/null 2>&1
+systemctl start kubelet >/dev/null 2>&1
+
+# Enable ssh password authentication
+echo "[TASK 11] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 12] Set root password"
+echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
+
+# Update vagrant user's bashrc file
+echo "export TERM=xterm" >> /etc/bashrc
